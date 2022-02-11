9c33715408913b1cc25976f00e7b1d272d3e68d2
MartinDevCan <Annamartini@mail.ru>
2022-02-11 16:12:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { data } from "./data";
+import './App.css'
+import { BsTrash } from "react-icons/bs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [gift, setGift] = useState(data);
+
+    const removeGift = (id) => {
+        let newGift = gift.filter(gift => gift.id !== id);
+        setGift(newGift);
+    }
+
+
+
+
+    return (<div>
+<div className='container'>
+    <h1>{gift.length} Great Romantic Date Ideas</h1>
+</div>
+
+{gift.map((gift => {
+    const {id, item, description, image} = gift;
+    return(
+        <div key={id}>
+            <div className='container'>
+                <h2>{id} - {item}</h2>
+            </div>
+
+            <div className='container'>
+                <p>{description}</p>
+            </div>
+
+            <div className='container'>
+                <img src={image} alt="gift idea"/>
+            </div>
+
+            <div className='container'>
+                <button className='trashIcon' onClick={() => removeGift(id)}> <BsTrash /></button>
+            </div>
+
+
+        </div>
+    )
+}))}
+<div className='container'>
+<button className='delAll' onClick={() => setGift([])}>Delete all</button>
+</div>
+    
+    </div>)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const data = [
+
+    {
+    id: 1,
+    item: "Hot Air Ballon Ride",
+    description: "Experience the magic of a hot air balloon ride while floating over beautiful Toronto.",
+    image: "https://images.unsplash.com/photo-1497531551184-06b252e1bee1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+    },
+
+    {
+    id: 2,
+    item: "Couples Massage",
+    description: "Eager to relax with your significant other? There's no better way to bond while allowing the stresses of your day-to-day life melt away than with a couples massage. With all that tension melting away, you'll be able to focus on what's really important: your relationship.",
+    image: "https://fuchsiaspa.com/wp-content/uploads/Fushcia_Slider_Couples-Massage.jpg"
+    },
+
+    {
+    id: 3,
+    item: "Picnic in the Park",
+    description: "If you're looking to spice things up, try treating your significant other to a picnic with a beautiful scenery and cracking open a bottle of bubbly to make it all the more romantic.",
+    image: "https://images.unsplash.com/photo-1549934159-af720506e2bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHBpY25pY3xlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+    id: 4,
+    item: "Dance Class for Two",
+    description: "Whether you've just started dating or have been together for years, learning the dance of love is a great way to get close to your partner, both physically and emotionally.",
+    image: "https://images.unsplash.com/photo-1575449235240-406e17a9df7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGFuZ298ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+        id: 5,
+        item: "Romantic Bike Trip",
+        description: "Explore the city with your best half. Take a romantic trip to the places you like.",
+        image: "https://images.pexels.com/photos/5226493/pexels-photo-5226493.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+
+    {
+        id: 6,
+        item: "Ice Skating Date",
+        description: "Holding hands while skating is one of the benefits of being on the cold ice. Gotta keep your date's hand nice and warm. Just make sure you don't fall on top of each other . . . or maybe you’d like it.",
+        image: "https://iceskatingpassion.com/wp-content/uploads/2020/10/ice-skating-first-date-skates.jpg"
+    },
+
+    {
+        id: 7,
+        item: "Romantic Road Trip",
+        description: "Take a road trip together to places that played an important part in your life.",
+        image: "https://images.unsplash.com/flagged/photo-1557772626-6534a2db6365?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+
+    {
+        id: 8,
+        item: "Trip to a winery",
+        description: "Ontario wineries are the perfect place to spend a romantic getaway. Landscape in our beautiful province makes a picturesque backdrop for romantic photos and memories with your sweetheart. With more than 150 wineries across the province, you can have your pick of amazing vineyards throughout Ontario.",
+        image: "https://images.unsplash.com/photo-1586762615173-27561b3efae5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHdpbmVyeSUyMGRhdGV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+
+    {
+        id: 9,
+        item: "Have fun at karaoke bar",
+        description: "The good, the bad, the good-bad. There's so many opportunities on this date to talk about favorite bands, musicians, and songs. This is a good chance to figure out what each other is into, without sounding like a music snob.",
+        image: "https://images.unsplash.com/photo-1638433665113-4d3eabf25d1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGthcmFva2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    }
+    
+]
